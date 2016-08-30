5b26e9ccdae8819ab5647c25e71348907d9b2a50
angular-cli <angular-cli@angular.io>
2016-08-30 13:58:16 +0200

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AmelieMailPage } from './app.po';
+
+describe('amelie-mail App', function() {
+  let page: AmelieMailPage;
+
+  beforeEach(() => {
+    page = new AmelieMailPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
---
+++ e2e/app.po.ts
@@ -0,0 +1,9 @@
+export class AmelieMailPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
