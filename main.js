efb14ea62cd935a132ded4659b090a793a062339
František Žiačik <fziacik@gratex.com>
2016-09-05 09:28:02 +0200

    Main dependencies added.

---
+++ main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow() {
+	mainWindow = new BrowserWindow();
+	mainWindow.loadURL(`file://${__dirname}/dist/index.html`);
+	mainWindow.on('closed', function() {
+		mainWindow = null;
+	});
+}
+app.on('ready', () => {
+	createWindow();
+});
+app.on('window-all-closed', function() {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+app.on('activate', function() {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
