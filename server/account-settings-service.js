408922e5c36f4b6cffdccdfe4feb3a342ad8c69b
František Žiačik <fziacik@gratex.com>
2016-10-14 17:38:11 +0200

    Account Settings Service added.

---
+++ server/account-settings-service.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const rx = require('rxjs/Observable');
+require('rxjs/add/observable/bindNodeCallback');
+require('rxjs/add/operator/map');
+
+class AccountSettingsService {
+	constructor(fs) {
+		this.fs = fs;
+	}
+
+	getAll() {
+		return rx.Observable.bindNodeCallback(this.fs.readFile)('/home/UX/fziacikux/.amelie-mail.accounts.json', 'utf8').map(data => JSON.parse(data));
+	}
+}
+
+module.exports = AccountSettingsService;
+module.exports['@singleton'] = true;
+module.exports['@require'] = ['fs'];
---
+++ server/account-settings-service.spec.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const rx = require('rxjs/Observable');
+require('rxjs/add/observable/of');
+
+const chai = require('chai');
+const sinon = require('sinon');
+const expect = chai.expect;
+chai.use(require('sinon-chai'));
+
+const AccountSetingsService = require('./account-settings-service');
+
+describe('Account Setings Service', () => {
+	let accountSetingsService;
+
+	let accountSettings = {
+		login: "some.login"
+	}
+
+	let fs = {
+		readFile: sinon.stub().yields(null, JSON.stringify(accountSettings))
+	};
+
+	beforeEach(() => {
+		accountSetingsService = new AccountSetingsService(fs);
+	});
+
+	describe('getAll', () => {
+		it('allows us to subscribe for the settings', () => {
+			let subscription = accountSetingsService.getAll().subscribe();
+			expect(subscription).to.exist;
+		});
+
+		it('loads a settings file and parses it into an object', () => {
+			return accountSetingsService.getAll().subscribe(result => {
+				expect(fs.readFile).to.have.been.calledWith('~/.amelie-mail.accounts.json', 'utf8');
+				expect(result).to.deep.equal(accountSettings);
+			});
+		});
+	});
+});
