16e99b3190d69596cd10122946c524fd9331fa8a
František Žiačik <ziacik@gmail.com>
2017-01-11 00:12:13 +0100

    chore: cli upgrade

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,30 +4,30 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('App: AmelieMail', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    });
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [
+				AppComponent
+			],
+		});
+	});
 
-  it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+	it('should create the app', async(() => {
+		let fixture = TestBed.createComponent(AppComponent);
+		let app = fixture.debugElement.componentInstance;
+		expect(app).toBeTruthy();
+	}));
 
-  it(`should have a title`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Amelie Mail');
-  }));
+	it(`should have a title`, async(() => {
+		let fixture = TestBed.createComponent(AppComponent);
+		let app = fixture.debugElement.componentInstance;
+		expect(app.title).toEqual('Amelie Mail');
+	}));
 
-  it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Amelie Mail');
-  }));
+	it('should render title in a h1 tag', async(() => {
+		let fixture = TestBed.createComponent(AppComponent);
+		fixture.detectChanges();
+		let compiled = fixture.debugElement.nativeElement;
+		expect(compiled.querySelector('h1').textContent).toContain('Amelie Mail');
+	}));
 });
