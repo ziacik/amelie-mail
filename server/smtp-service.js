ecce723b44af8dd8967cbd6a24cc29cba13f8333
František Žiačik <ziacik@gmail.com>
2017-02-07 00:44:45 +0100

    Switched from emailjs smtp to nodemailer.

--- server/smtp-service.js
+++ server/smtp-service.js
@@ -5,12 +5,12 @@ require('./rxjs-operators');
 const Rx = require('rxjs');
 
 class SmtpService {
-	constructor(accountSettingsService, Client) {
+	constructor(accountSettingsService, client) {
 		this.errors = {
 			mailArgumentMissing: 'Mail argument missing',
 			errorSendingMail: 'Unable to send a mail'
 		};
-		this.Client = Client;
+		this.client = client;
 		this.accountSettingsService = accountSettingsService;
 	}
 
@@ -19,86 +19,29 @@ class SmtpService {
 			throw new Error(this.errors.mailArgumentMissing);
 		}
 
-		return this._createClient().flatMap(client => {
-			return Rx.Observable.merge(
-				this._watchForErrors(client),
-				this._connect(client),
-				this._onIdleSendEnvelope(client, mail),
-				this._onReadySendMail(client, mail)
-			).takeUntil(this._onDoneFinish(client));
-		});
+		return this._createTransport().flatMap(transport => this._sendMail(transport, mail))
 	}
 
-	_createClient() {
+	_createTransport() {
 		return this.accountSettingsService.getAll().map(accountSettings => {
-			let client = new this.Client(accountSettings.smtp.host, accountSettings.smtp.port, accountSettings.smtp.options);
-			client.from = accountSettings.mailAddress;
-			return client;
-		});
-	}
-
-	_connect(client) {
-		return Rx.Observable.defer(() => Rx.Observable.of(client.connect()));
-	}
-
-	_watchForErrors(client) {
-		let errorObservable = Rx.Observable.fromEventPattern(
-				handler => client.onerror = handler,
-				() => delete client.onerror
-			)
-			.first();
-
-		let closeObservable = Rx.Observable.fromEventPattern(
-				handler => client.onclose = handler,
-				() => delete client.onclose
-			)
-			.map(() => new Error('Unexpected close'))
-			.first();
-
-		return Rx.Observable.merge(errorObservable, closeObservable).flatMap(err => Rx.Observable.throw(err));
-	}
-
-	_onIdleSendEnvelope(client, mail) {
-		return Rx.Observable.fromEventPattern(
-			handler => client.onidle = handler,
-			() => delete client.onidle
-		).first().map(() => {
-			client.useEnvelope({
-				from: client.from,
-				to: (mail.to || []).concat(mail.cc || []).concat(mail.bcc || [])
+			return this.client.createTransport(accountSettings.smtp, {
+				from: `"${accountSettings.name}" <${accountSettings.mailAddress}>`
 			});
-		})
-	}
-
-	_onReadySendMail(client, mail) {
-		return Rx.Observable.fromEventPattern(
-			handler => client.onready = handler,
-			() => delete client.onready
-		).first().map(() => {
-			client.send(`Subject: ${mail.subject}\n`);
-			client.send('Mime-Version: 1.0;\n');
-			client.send('Content-Type: text/html; charset="ISO-8859-1";\n');
-			client.send('Content-Transfer-Encoding: 7bit;\n');
-			client.send('\n');
-			client.send(mail.content);
-			client.end();
 		});
 	}
 
-	_onDoneFinish(client) {
-		return Rx.Observable.fromEventPattern(
-			handler => client.ondone = handler,
-			() => delete client.ondone
-		).first().flatMap(result => {
-			if (result) {
-				return Rx.Observable.of(null);
-			} else {
-				return Rx.Observable.throw(new Error(this.errors.errorSendingMail));
-			}
-		})
+	_sendMail(transport, mail) {
+		let sendMail = Rx.Observable.bindNodeCallback(transport.sendMail.bind(transport));
+		return sendMail({
+			to: mail.to,
+			cc: mail.cc,
+			bcc: mail.bcc,
+			subject: mail.subject,
+			html: mail.content
+		});
 	}
 }
 
 module.exports = SmtpService;
 module.exports['@singleton'] = true;
-module.exports['@require'] = ['account-settings-service', 'emailjs-smtp-client'];
+module.exports['@require'] = ['account-settings-service', 'nodemailer'];
--- server/smtp-service.spec.js
+++ server/smtp-service.spec.js
@@ -14,24 +14,29 @@ const SmtpService = require('./smtp-service');
 
 describe('Smtp Service', () => {
 	let smtpService;
-	let ClientClass;
 	let client;
+	let transport;
 	let mail;
 
 	let accountSettingsService;
 	let accountSettingsObservable;
 	let accountSettings = {
 		mailAddress: 'my@mail',
+		name: 'This Me',
 		smtp: {
 			host: 'some.host',
 			port: 25,
-			options: {
+			auth: {
 				some: 'thing'
 			}
 		}
 	};
 
 	beforeEach(() => {
+		transport = {
+			sendMail: sinon.stub()
+		};
+
 		mail = {
 			to: ['some@body'],
 			cc: ['cc@body'],
@@ -41,18 +46,13 @@ describe('Smtp Service', () => {
 		};
 
 		client = {
-			connect: sinon.stub(),
-			useEnvelope: sinon.stub(),
-			send: sinon.stub(),
-			end: sinon.stub()
+			createTransport: sinon.stub().returns(transport)
 		};
-
-		ClientClass = sinon.stub().returns(client);
 
 		accountSettingsObservable = rx.Observable.of(accountSettings);
 		accountSettingsService = {};
 		accountSettingsService.getAll = sinon.stub().returns(accountSettingsObservable);
-		smtpService = new SmtpService(accountSettingsService, ClientClass);
+		smtpService = new SmtpService(accountSettingsService, client);
 	});
 
 	describe('send', () => {
@@ -69,232 +69,45 @@ describe('Smtp Service', () => {
 		it('sets up the client with account settings', done => {
 			smtpService.send(mail).subscribe();
 			setTimeout(() => {
-				expect(ClientClass).to.have.been.calledWith('some.host', 25, {
-					some: 'thing'
+				expect(client.createTransport).to.have.been.calledWith({
+					host: 'some.host',
+					port: 25,
+					auth: {
+						some: 'thing'
+					}
+				}, {
+					from: '"This Me" <my@mail>'
 				});
 				done();
 			}, 20);
 		});
 
-		it('connects', done => {
+		it('sends the mail', done => {
 			smtpService.send(mail).subscribe();
 			setTimeout(() => {
-				expect(client.connect).to.have.been.called;
+				expect(transport.sendMail).to.have.been.calledWith(sinon.match({
+					to: ['some@body'],
+					cc: ['cc@body'],
+					bcc: ['bcc@body'],
+					subject: 'A subject',
+					html: 'mail body'
+				}), sinon.match.any);
 				done();
 			}, 20);
 		});
 
-		it('must already be subscribed to onerror and onclose on connect', done => {
-			client.connect = () => {
-				expect(client.onerror).to.exist;
-				expect(client.onclose).to.exist;
+		it('completes if sendMail ends without an error', done => {
+			transport.sendMail.yields(null);
+			smtpService.send(mail).subscribe(() => {
 				done();
-			};
-			smtpService.send(mail).subscribe();
+			}, done);
 		});
 
-		it('subscribes to an onerror event', done => {
-			smtpService.send(mail).subscribe();
-			setTimeout(() => {
-				expect(client.onerror).to.exist;
+		it('completes with error if sendMail ends with error', done => {
+			transport.sendMail.yields(new Error('Some error'));
+			smtpService.send(mail).subscribe(() => done(new Error('Should have failed.')), e => {
+				expect(e.message).to.equal('Some error');
 				done();
-			}, 20);
-		});
-
-		it('subscribes to an onclose event', done => {
-			smtpService.send(mail).subscribe();
-			setTimeout(() => {
-				expect(client.onclose).to.exist;
-				done();
-			}, 20);
-		});
-
-		it('subscribes to an onidle event', done => {
-			smtpService.send(mail).subscribe();
-			setTimeout(() => {
-				expect(client.onidle).to.exist;
-				done();
-			}, 20);
-		});
-
-		it('does not send envelope if onidle event didnt occur', done => {
-			smtpService.send(mail).subscribe();
-			setTimeout(() => {
-				expect(client.useEnvelope).not.to.have.been.called;
-				done();
-			}, 20);
-		});
-
-		it('completes with error if onerror event occurs', done => {
-			let error;
-			setTimeout(() => client.onerror && client.onerror(new Error('Some error')), 1);
-			setTimeout(() => client.onclose && client.onclose(true), 2);
-			smtpService.send(mail).subscribe(null, e => error = e);
-			setTimeout(() => {
-				expect(error).to.exist;
-				expect(error).to.be.an('error');
-				expect(error.message).to.equal('Some error');
-				done();
-			}, 20);
-		});
-
-		it('completes with error if unexpected onclose event occurs', done => {
-			let error;
-			setTimeout(() => client.onclose && client.onclose(false), 1);
-			smtpService.send(mail).subscribe(null, e => error = e);
-			setTimeout(() => {
-				expect(error).to.exist;
-				expect(error).to.be.an('error');
-				expect(error.message).to.equal('Unexpected close');
-				done();
-			}, 20);
-		});
-
-		describe('after onidle event', () => {
-			beforeEach(() => {
-				smtpService.Client = sinon.spy(() => {
-					setTimeout(() => client.onidle && client.onidle(), 1);
-					return client;
-				});
-			});
-
-			it('sends an envelope', done => {
-				smtpService.send(mail).subscribe();
-				setTimeout(() => {
-					expect(client.useEnvelope).to.have.been.calledWith({
-						from: 'my@mail',
-						to: ['some@body', 'cc@body', 'bcc@body']
-					});
-					done();
-				}, 20);
-			});
-
-			it('only sends envelope once when multiple onidle events occur', done => {
-				smtpService.Client = sinon.spy(() => {
-					setTimeout(() => client.onidle(), 1);
-					setTimeout(() => client.onidle && client.onidle(), 2);
-					setTimeout(() => client.onidle && client.onidle(), 4);
-					return client;
-				});
-				smtpService.send(mail).subscribe();
-				setTimeout(() => {
-					expect(client.useEnvelope).to.have.been.calledWith({
-						from: 'my@mail',
-						to: ['some@body', 'cc@body', 'bcc@body']
-					});
-					done();
-				}, 20);
-			});
-
-			it('subscribes to onready event', done => {
-				smtpService.send(mail).subscribe();
-				setTimeout(() => {
-					expect(client.onready).to.exist;
-					done();
-				}, 20);
-			});
-
-			it('does not send a mail if onready event didnt occur', done => {
-				smtpService.send(mail).subscribe();
-				setTimeout(() => {
-					expect(client.send).not.to.have.been.called;
-					done();
-				}, 20);
-			});
-
-			it('completes with error if onerror event occurs', done => {
-				let error;
-				setTimeout(() => client.onerror && client.onerror(new Error('Some error')), 1);
-				setTimeout(() => client.onclose && client.onclose(true), 2);
-				smtpService.send(mail).subscribe(null, e => error = e);
-				setTimeout(() => {
-					expect(error).to.exist;
-					expect(error).to.be.an('error');
-					expect(error.message).to.equal('Some error');
-					done();
-				}, 20);
-			});
-
-			describe('after onready event', () => {
-				beforeEach(() => {
-					client.useEnvelope = () => {
-						setTimeout(() => client.onready && client.onready(), 1);
-					};
-				});
-
-				it('sends a mail and calls end', done => {
-					smtpService.send(mail).subscribe();
-					setTimeout(() => {
-						expect(client.send).to.have.been.calledWith('Subject: A subject\n');
-						expect(client.send).to.have.been.calledWith('\n');
-						expect(client.send).to.have.been.calledWith('mail body');
-						expect(client.end).to.have.been.called;
-						done();
-					}, 20);
-				});
-
-				it('subscribes to ondone event', done => {
-					smtpService.send(mail).subscribe();
-					setTimeout(() => {
-						expect(client.ondone).to.exist;
-						done();
-					}, 20);
-				});
-
-				it('does not complete the subscription if ondone event didnt occur', done => {
-					let completed = 'not completed';
-					smtpService.send(mail).subscribe(null, done, () => completed = 'completed');
-					setTimeout(() => {
-						expect(completed).to.equal('not completed');
-						done();
-					}, 20);
-				});
-
-				it('completes with error if onerror event occurs', done => {
-					let error;
-					setTimeout(() => client.onerror && client.onerror(new Error('Some error')), 1);
-					setTimeout(() => client.onclose && client.onclose(true), 2);
-					smtpService.send(mail).subscribe(null, e => error = e);
-					setTimeout(() => {
-						expect(error).to.exist;
-						expect(error).to.be.an('error');
-						expect(error.message).to.equal('Some error');
-						done();
-					}, 20);
-				});
-
-				describe('after ondone event', () => {
-					let onDoneResult;
-
-					beforeEach(() => {
-						onDoneResult = true;
-						client.end = () => {
-							setTimeout(() => client.ondone && client.ondone(onDoneResult), 1);
-							setTimeout(() => client.onclose && client.onclose(false), 2);
-						};
-					});
-
-					it('completes without error if onDone triggers with true', done => {
-						let completed = 'not completed';
-						smtpService.send(mail).subscribe(null, done, () => completed = 'completed');
-						setTimeout(() => {
-							expect(completed).to.equal('completed');
-							done();
-						}, 20);
-					});
-
-					it('completes with error if onDone triggers with false', done => {
-						let error;
-						onDoneResult = false;
-						smtpService.send(mail).subscribe(null, e => error = e);
-						setTimeout(() => {
-							expect(error).to.exist;
-							expect(error).to.be.an('error');
-							expect(error.message).to.equal(smtpService.errors.errorSendingMail || '(error not defined)');
-							done();
-						}, 20);
-					});
-				});
 			});
 		});
 	});
