0cf72dd310ca9ddb2c574b99ac77f429035bf86d
František Žiačik <ziacik@gmail.com>
2017-03-05 18:11:25 +0100

    Stuff regenerated with new cli.

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,7 @@
-// Typings reference file, you can add your own global typings here
-// https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
+/* SystemJS module definition */
+declare var module: {
+	id: string;
+};
 
 declare var System: any;
 declare var tinymce: any;
