d4d33d0cfa884620981a6ed5ae2214f0d1321235
František Žiačik <ziacik@gmail.com>
2017-03-27 22:16:05 +0200

    test: Editor coverage raised.

--- src/app/mail-editor/mail-editor.component.spec.ts
+++ src/app/mail-editor/mail-editor.component.spec.ts
@@ -10,6 +10,9 @@ declare var tinymce;
 describe('MailEditorComponent', () => {
 	let component: MailEditorComponent;
 	let fixture: ComponentFixture<MailEditorComponent>;
+	let tinyMceOptions: any;
+	let editor: any;
+	let changeCallback: any;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
@@ -18,8 +21,20 @@ describe('MailEditorComponent', () => {
 	}));
 
 	beforeEach(() => {
+		editor  = {
+			setContent: jasmine.createSpy('setContent'),
+			getContent: jasmine.createSpy('getContent').and.returnValue('Some content.'),
+			on: jasmine.createSpy('on').and.callFake((evt, callback) => {
+				if (evt === 'change') {
+					changeCallback = callback;
+				}
+			})
+		};
 		tinymce = {
-			init: jasmine.createSpy('init'),
+			init: jasmine.createSpy('init').and.callFake(opt => {
+				tinyMceOptions = opt;
+				tinyMceOptions.setup(editor);
+			}),
 			remove: jasmine.createSpy('remove')
 		};
 		fixture = TestBed.createComponent(MailEditorComponent);
@@ -34,5 +49,16 @@ describe('MailEditorComponent', () => {
 	it('should remove tinymce in OnDestroy', () => {
 		component.ngOnDestroy();
 		expect(tinymce.remove).toHaveBeenCalled();
+	});
+
+	it('sets a value to tinymce when value is set on editor', () => {
+		component.value = 'Some value.';
+		expect(editor.setContent).toHaveBeenCalledWith('Some value.');
+	});
+
+	it('retrieves a value from tinymce when change event occurs', () => {
+		changeCallback();
+		expect(editor.getContent).toHaveBeenCalled();
+		expect(component.value).toEqual('Some content.');
 	});
 });
